f5137d01b312ea4f226ba6d6ebbd1baf9d4772b1
Sayantansen123 <riosayan123@gmail.com>
2024-11-26 08:47:51 +0530

    image slider added

---
+++ src/components/Residencies/Residencies.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Residencies.css"
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
+import "swiper/css"
+import data from '../../utils/slider.json'
+
+const Residencies = () => {
+  return (
+    <section className="r-wrapper">
+      <div className="paddings innerWidth r-container">
+        <div className="r-head flexColStart">
+          <span className='orangeText'>Best Choices</span>
+          <span className='primaryText'>Popular Residencies</span>
+        </div>
+
+        <Swiper>
+          {
+            data.map((card,i)=>(
+              <SwiperSlide></SwiperSlide>
+            ))
+
+          }
+
+        </Swiper>
+      </div>
+    </section>
+  )
+}
+
+export default Residencies
