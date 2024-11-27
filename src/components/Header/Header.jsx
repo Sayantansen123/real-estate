a0de3cf1c1739072be90f34484e50412b806b848
Sayantansen123 <riosayan123@gmail.com>
2024-11-28 04:37:46 +0530

    header content updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,36 @@
 import React, { useState } from 'react'
-import "./Header.css"
 import { BiMenuAltRight } from 'react-icons/bi'
 import OutsideClickHandler from 'react-outside-click-handler';
+import "./Header.css"
+
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-
-  const getMenuSTyle = (menuOpen)=>{
-    if(document.documentElement.clientWidth <= 800){
-      return {right : !menuOpen && "-100%"}
+  const getMenuSTyle = (menuOpen) => {
+    if (document.documentElement.clientWidth <= 800) {
+      return { right: !menuOpen && "-100%" }
     }
   }
+
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
         <img src="./logo.png" alt="logo" width={50} />
 
-        <OutsideClickHandler onOutsideClick={()=>setMenuOpen(false)}>
-        <div className="flexCenter h-menu " style={getMenuSTyle(menuOpen)}>
-          <a href="">Residencies</a>
-          <a href="">Our Value</a>
-          <a href="">Contact Us</a>
-          <a href="">Get Started</a>
-          <button className='button'>
-            <a href="">Contact</a>
-          </button>
-        </div>
-        
-        <div className="menu-icon" onClick={()=>{setMenuOpen((prev) => !prev)}}>
-          <BiMenuAltRight size={30} />
-
-        </div>
+        <OutsideClickHandler onOutsideClick={() => setMenuOpen(false)}>
+          <div className="flexCenter h-menu " style={getMenuSTyle(menuOpen)}>
+            <a href="">Residencies</a>
+            <a href="">Our Value</a>
+            <a href="">Contact Us</a>
+            <a href="">Get Started</a>
+            <button className='button'>
+              <a href="">Contact</a>
+            </button>
+          </div>
+          <div className="menu-icon" onClick={() => { setMenuOpen((prev) => !prev) }}>
+            <BiMenuAltRight size={30} />
+          </div>
         </OutsideClickHandler>
       </div>
-
-
     </section>
   )
 }
