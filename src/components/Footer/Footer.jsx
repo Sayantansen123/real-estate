10d23206c158061813f8273f2ad3bb7b53698604
Sayantansen123 <riosayan123@gmail.com>
2024-11-27 17:16:32 +0530

    footer completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,29 @@ import "./Footer.css"
 const Footer = () => {
   return (
     <section className="f-wrapper">
-        <div className='paddings innerWidth flex-center f-container'>
-            
+      <div className='paddings innerWidth flexCenter f-container'>
+        <div className="flexColStart f-left">
+          <img src="./logo.png" alt="" width={100} />
+
+          <span className="secondaryText">
+            Our vision is to make all people <br />
+            the best place to live for them
+          </span>
         </div>
+
+        <div className="flexColStart f-right">
+          <span className='primaryText'>Information</span>
+          <span className='secondaryText'>145 kolkata, Fl 457, India</span>
+
+          <div className="flexCenter f-menu">
+               <span>Property</span>
+               <span>Services</span>
+               <span>Product</span>
+               <span>About Us</span>
+          </div>
+        </div>
+
+      </div>
     </section>
   )
 }
